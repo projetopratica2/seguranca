ef85591bcea6b6153fcf2e7ba6105a793bc81a60
Rodrigo <rodriglesias@msn.com>
2016-05-03 17:00:55 -0300

    testando primeiro envio

---
+++ login/index.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+<title>Login</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+<body>
+<form name="form1" method="post" action="enviar.php">
+<p>Login:
+<input name="login" type="text" id="login">
+</p>
+<p>Senha:
+<input name="senha" type="password" id="senha">
+</p>
+<p>
+<input type="submit" name="Submit" value="Entrar">
+</p>
+<p ahref="">Esqueceu sua senha?</p>
+</form>
+</body>
+</html>
+
